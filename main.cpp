d9e8d2b76bf13888f44c0da203e4e4171ecc180f
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2024-11-26 11:19:53 +0700

    DSLK int

--- main.cpp
+++ main.cpp
@@ -1,29 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct DonThuc {
-    int CoSo;
-    int SoMu;
-}DonThuc;
 typedef struct Node {
-    DonThuc info;
+    int info;
     struct Node *pNext;
 }NODE;
 typedef NODE* NODEPTR;
-void Input(DonThuc &dt) {
-    scanf("%d%d", &dt.CoSo, &dt.SoMu);
-}
-void Output(DonThuc dt) {
-    printf("(%d)x^%d+", dt.CoSo,dt.SoMu);
-}
 
-NODEPTR createNode(DonThuc input) {
+NODEPTR createNode(int input) {
     NODEPTR newNode = new NODE;
     newNode->info = input;
     newNode->pNext = NULL;
     return newNode;
 }
-void Insert(NODEPTR &pHead, DonThuc input) {
+void Insert(NODEPTR &pHead, int input) {
     if (pHead == NULL) {
         pHead = createNode(input);
         return;
@@ -32,25 +22,84 @@ void Insert(NODEPTR &pHead, DonThuc input) {
     newNode->pNext = pHead;
     pHead = newNode;
 }
+
+void InsertTail(NODEPTR &pHead, int input) {
+    if(pHead == NULL) {
+        Insert(pHead, input);
+    }
+    NODEPTR current = pHead;
+    while (current->pNext != NULL) {
+        current = current->pNext;
+    }
+    current->pNext = createNode(input);
+}
+
+void InsertPositionK(NODEPTR &pHead, int input,
+    int position) {
+    if (pHead == NULL) {
+        Insert(pHead, input);
+        return;
+    }
+    NODEPTR current = pHead;
+    int i = 0;
+    while (i < position && current->pNext != NULL) {
+        current = current->pNext;
+    }
+    if(i<position) {
+        InsertTail(pHead, input);
+        return;
+    }
+    NODEPTR newNode = createNode(input);
+    newNode->pNext = current->pNext;
+    current->pNext = newNode;
+}
+void RemoveHead(NODEPTR &pHead) {
+    if (pHead == NULL||pHead->pNext == NULL) {
+        pHead = NULL;
+        return;
+    }
+    NODEPTR current = pHead;
+    pHead = pHead->pNext;
+    delete current;
+}
+void RemoveTail(NODEPTR &pHead) {
+    if (pHead == NULL||pHead->pNext == NULL) {
+        pHead = NULL;
+        return;
+    }
+    NODEPTR current = pHead;
+    while (current->pNext->pNext != NULL) {
+        current = current->pNext;
+    }
+    current->pNext = NULL;
+}
+//
+void Sort(NODEPTR &pHead) {
+    for (NODEPTR i = pHead; i->pNext!=NULL; i=i->pNext) {
+        for (NODEPTR j = i->pNext; j!=NULL; j=j->pNext) {
+            if(i->info > j->info) {
+                int temp = j->info;
+                j->info = i->info;
+                i->info = temp;
+            }
+        }
+    }
+}
+
 void Print(NODEPTR pHead) {
     NODEPTR curret = pHead;
     for (; curret!=NULL; curret=curret->pNext) {
-        Output(curret->info);
+        printf("%d\t", curret->info);
     }
 }
+
 int main() {
     NODEPTR pHead = NULL;
-    for (int i = 0; i < 5; ++i) {
-        DonThuc dt;
-        if(i%2==0) {
-            dt.SoMu = i;
-            dt.CoSo = i;
-        }else {
-            dt.SoMu = i;
-            dt.CoSo = i*-1;
-        }
-        Insert(pHead, dt);
-    }
+    Insert(pHead, 1);
+    Insert(pHead, 2);
+    Insert(pHead, 3);
+    Insert(pHead, 4);
+    Insert(pHead, 5);
     Print(pHead);
     return 0;
 }
